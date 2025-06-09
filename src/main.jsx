8efad3f913271d5b35c4540c16b7ac1aecfad629
Harshad313D <hdexamples@gmail.com>
2025-06-09 15:27:48 +0530

    seo

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { HelmetData } from 'react-helmet-async'
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <HelmetProvide>
+      <App />
+    </HelmetProvide>
+  </StrictMode>
+);
